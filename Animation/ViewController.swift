b407cff9dfa02803a18b4dbb1acd3de82d222007
itmisko <msi.mail@yandex.ru>
2022-08-04 14:00:10 +0300

    Initial Commit

---
+++ Animation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animation
+//
+//  Created by Sergei Mishchenko on 04.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
